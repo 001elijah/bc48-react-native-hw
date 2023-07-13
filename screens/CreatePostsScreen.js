a7f50892641ce0a6bcf60e88454634b51d9b8568
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-07-13 11:55:52 +0300

    add functionality to CreatePostsScreen

--- screens/CreatePostsScreen.js
+++ screens/CreatePostsScreen.js
@@ -1,18 +1,42 @@
-import { useEffect, useLayoutEffect, useState, createRef } from 'react';
-import { Keyboard, TouchableWithoutFeedback, KeyboardAvoidingView, Dimensions, StyleSheet, View, ScrollView, Image, Text, TextInput, TouchableOpacity } from 'react-native';
-import { useHeaderHeight } from '@react-navigation/elements'
+import { useEffect, useLayoutEffect, useState, createRef, useRef } from 'react';
+import { 
+  Keyboard, 
+  TouchableWithoutFeedback, 
+  KeyboardAvoidingView, 
+  Dimensions, 
+  StyleSheet, 
+  View, 
+  ScrollView, 
+  Image, 
+  Text, 
+  TextInput, 
+  TouchableOpacity } from 'react-native';
+import { useHeaderHeight } from '@react-navigation/elements';
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import * as Location from 'expo-location';
 import ArrowLeft from '../assets/icons/ArrowLeft';
 import placeholderImageSource from '../assets/images/avatar-placeholder.png';
 import MapPin from '../assets/icons/MapPin';
-import Camera from '../assets/icons/Camera';
+import { MaterialIcons } from '@expo/vector-icons';
+import CameraIcon from '../assets/icons/CameraIcon';
 import Trash from '../assets/icons/Trash';
 
 export default function CreatePostsScreen({ navigation }) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
   const [title, setTitle] = useState('');
-  const [location, setLocation] = useState('');
+  const [locationTitle, setLocationTitle] = useState('');
+  const [location, setLocation] = useState(null);
+  const [photo, setPhoto] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   const titleInputRef = createRef();
-  const locationInputRef = createRef();
+  const locationTitleInputRef = createRef();
+  // const cameraRef = createRef();
+  
+  const height = useHeaderHeight();
 
   useLayoutEffect(() => {
     const handleGoBack = () => {
@@ -28,25 +52,102 @@ export default function CreatePostsScreen({ navigation }) {
                     </TouchableOpacity>
             ),
         })
+  })
+  
+  useEffect(() => {
+    (async () => {
+      const { status: cameraStatus } = await Camera.requestCameraPermissionsAsync();
+      await MediaLibrary.requestPermissionsAsync();
+
+      setHasPermission(cameraStatus === "granted");
+
+      
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  function toggleCameraType() {
+    setType(current => (current === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back));
+  }
+
+  function removeCurrentPhoto() {
+    setPhoto(null);
+  }
+
+  const handleSubmitButton = async () => {
+    let { status: locationStatus } = await Location.requestForegroundPermissionsAsync();
+    if (locationStatus !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      text = errorMsg;
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+
+    if (!photo) {
+        alert('Please make a photo');
+        return;
+    }
+    if (!title) {
+        alert('Please fill title');
+        return;
+    }
+    if (!locationTitle) {
+        alert('Please fill location');
+        return;
+    }
+    const dataToSend = {
+      title,
+      locationTitle,
+      location: JSON.stringify(location),
+      photo
+    };
+    await MediaLibrary.createAssetAsync(photo);
+    console.log(dataToSend);
+    navigation.navigate("Home", {
+      screen: "PostsScreen"
     })
+  };
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS == "ios" ? "padding" : "height"}
       style={{ flex: 1 }}
-      keyboardVerticalOffset={useHeaderHeight()}
+      keyboardVerticalOffset={height}
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <ScrollView style={styles.container}>
-          <View>
-            <Image source={placeholderImageSource} style={styles.imageStyle} />
-            <TouchableOpacity
-                            style={styles.cameraButtonStyle}
-                            activeOpacity={0.5}
-                            onPress={()=> alert('something')}>
-                            <Camera />
-            </TouchableOpacity>
-          </View>
-          <Text style={styles.hintMessage}>Завантажте фото</Text>
+          {!photo ? (<Camera style={styles.camera} type={type} ref={setCameraRef}>
+            <View style={styles.cameraWrapper}>
+              <TouchableOpacity style={styles.flipCameraButtonStyle} onPress={toggleCameraType}>
+                <MaterialIcons name="flip-camera-ios" size={18} color="#fff" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.cameraButtonStyle}
+                onPress={async () => {
+                if (cameraRef) {
+                  const { uri } = await cameraRef.takePictureAsync();
+                  setPhoto(uri);
+                }
+            }}>
+                <CameraIcon />
+              </TouchableOpacity>
+            </View>
+          </Camera>) :
+          <Image source={{uri: photo}} style={styles.imageStyle} />
+          }
+          <TouchableOpacity
+            onPress={removeCurrentPhoto}
+          >
+            <Text style={styles.hintMessage}>{!photo ? 'Завантажте фото' : 'Редагувати фото'}</Text>
+          </TouchableOpacity>
           <TextInput
             style={[styles.postDataInputStyle, styles.titleInputWidth]}
             onChangeText={(title) => setTitle(title)}
@@ -58,31 +159,31 @@ export default function CreatePostsScreen({ navigation }) {
             blurOnSubmit={false}
           />
           <View style={styles.iconAndInputWrapper}>
-            <View style={[styles.postDataInputStyle, styles.locationInputStyle]}>
+            <View style={[styles.postDataInputStyle, styles.locationTitleInputStyle]}>
               <MapPin />
             </View>
             <TextInput
-              style={[styles.postDataInputStyle, styles.locationInputStyle, styles.locationInputWidth]}
-              onChangeText={(location) => setLocation(location)}
+              style={[styles.postDataInputStyle, styles.locationTitleInputStyle, styles.locationTitleInputWidth]}
+              onChangeText={(locationTitle) => setLocationTitle(locationTitle)}
               underlineColorAndroid="#f000"
               placeholder="Місцевість…"
               placeholderTextColor="#BDBDBD"
-              ref={locationInputRef}
+              ref={locationTitleInputRef}
               returnKeyType="next"
               blurOnSubmit={false}
             />
           </View>
           <TouchableOpacity
-                            style={styles.buttonStyle}
-                            activeOpacity={0.5}
-                            onPress={()=> alert('something')}>
-                            <Text style={styles.buttonTextStyle}>Опублікувати</Text>
+            style={[styles.buttonStyle, photo && title && locationTitle && styles.readyToPuplish]}
+            activeOpacity={0.5}
+            onPress={handleSubmitButton}>
+            <Text style={[styles.buttonTextStyle, photo && title && locationTitle && styles.readyToPuplish]}>Опублікувати</Text>
           </TouchableOpacity>
           <TouchableOpacity
-                            style={styles.removeButtonStyle}
-                            activeOpacity={0.5}
-                            onPress={()=> alert('something')}>
-                            <Trash />
+            style={styles.removeButtonStyle}
+            activeOpacity={0.5}
+            onPress={()=> alert('something')}>
+            <Trash />
           </TouchableOpacity>
         </ScrollView>
       </TouchableWithoutFeedback>
@@ -100,11 +201,30 @@ const styles = StyleSheet.create({
   goBackButton: {
     marginLeft: 16,
   },
+  camera: {
+    flex: 1,
+    borderRadius: 8,
+    overflow: 'hidden',
+  },  
+  cameraWrapper: {
+    height: 240,
+    width: Dimensions.get('window').width - 32,
+  },
   imageStyle: {
     height: 240,
     width: Dimensions.get('window').width - 32,
-    paddingBottom: 8,
     borderRadius: 8,
+  },
+  flipCameraButtonStyle: {
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 3,
+    left: (Dimensions.get('window').width - 65),
+    borderRadius: 50,
+    backgroundColor: 'rgba(255, 255, 255, 0.30)',
   },
   cameraButtonStyle: {
     width: 60,
@@ -115,9 +235,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 50,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(255, 255, 255, 0.30)',
   },
   hintMessage: {
+    paddingTop: 8,
     paddingBottom: 32,
     color: '#BDBDBD',
     fontSize: 16,
@@ -134,14 +255,14 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#E8E8E8',
   },
-  locationInputStyle: {
+  locationTitleInputStyle: {
     justifyContent: 'center',
     paddingRight: 4,
   },
   titleInputWidth: {
     width: Dimensions.get('window').width - 32,
   },
-  locationInputWidth: {
+  locationTitleInputWidth: {
     width: Dimensions.get('window').width - 56,
   },
   buttonStyle: {
@@ -153,6 +274,10 @@ const styles = StyleSheet.create({
     borderWidth: 0,
     borderColor: '#FF6C00',
     borderRadius: 100,
+  },
+  readyToPuplish: {
+    backgroundColor: '#FF6C00',
+    color: '#fff'
   },
   buttonTextStyle: {
     color: '#BDBDBD',
